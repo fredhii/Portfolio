c67ce918052a970dab476a7ffd668414388b4b76
Fredhii <fredhiixd@gmail.com>
2024-10-30 11:32:04 -0500

    feat: add installation guide for Coolify in MDX format to enhance documentation chore: update .gitignore to exclude robots.txt and sitemap files for cleaner repository feat: add next-sitemap configuration file to automate sitemap generation chore: update package.json to include next-sitemap in postbuild script for automatic sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://fredhii.com', // replace with your actual site URL
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+};
